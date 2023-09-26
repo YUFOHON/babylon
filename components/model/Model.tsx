65d05274abbeaf661c91c0f711d919c022270d26
Yu Fo Hon <117070313+YUFOHON@users.noreply.github.com>
2023-09-26 22:18:45 +0800

    loadingBar

--- components/model/Model.tsx
+++ components/model/Model.tsx
@@ -1,6 +1,6 @@
 import React, {FunctionComponent, useEffect, useState} from 'react';
 import {Platform} from 'react-native';
-import {SafeAreaView, View, ViewProps} from 'react-native';
+import {SafeAreaView,Text, View, ViewProps, ActivityIndicator} from 'react-native';
 import {EngineView, useEngine} from '@babylonjs/react-native';
 import {SceneLoader} from '@babylonjs/core/Loading/sceneLoader';
 import {Camera} from '@babylonjs/core/Cameras/camera';
@@ -13,11 +13,9 @@ import {
   Color4,
   Vector3,
 } from '@babylonjs/core';
-
-import {ReactNativeEngine} from '@babylonjs/react-native/ReactNativeEngine.js';
-
 const isAndroid = Platform.OS === 'android';
 const assetPrefix = isAndroid ? 'custom/' : '';
+let isLoading = true;
 
 function resolveAssetUri(path: string) {
   return `app:///${assetPrefix}${path}`;
@@ -38,94 +36,114 @@ class CustomLoadingScreen implements ILoadingScreen {
   constructor(public loadingUIText: string) {}
   public displayLoadingUI() {
     console.warn(this.loadingUIText);
+    // <Ionicons name="md-checkmark-circle" size={32} color="green" />
   }
 
   public hideLoadingUI() {
-    console.warn("Loaded!");
+    console.warn('Loaded!');
+    isLoading = false;
   }
 }
 const EngineScreen: FunctionComponent<ViewProps> = (props: ViewProps) => {
-  // const engine =new ReactNativeEngine()
-  const engine =  useEngine();
-
+  const engine = useEngine();
   const [camera, setCamera] = useState<Camera>();
   const [scene, setScene] = useState<Scene>();
 
   useEffect(() => {
-      console.log('enter model component');
-      if (engine) {
-        // console.log('🚀 ~ file: model.tsx:27 ~ useEffect ~ engine:', engine);
-        var loadingScreen = new CustomLoadingScreen("I'm loading!!");
-        engine.loadingScreen = loadingScreen;
-        engine.displayLoadingUI();
-        const girl =
-          // 'https://raw.githubusercontent.com/YUFOHON/babylon/main/assets/girl/girl_laugh_idle_angry_clapping.glb';
-          'https://raw.githubusercontent.com/YUFOHON/babylon/main/assets/girl/realGirl.glb';
+    console.log('enter model component');
+    if (engine) {
+      // console.log('🚀 ~ file: model.tsx:27 ~ useEffect ~ engine:', engine);
+      var loadingScreen = new CustomLoadingScreen("I'm loading!!");
+      engine.loadingScreen = loadingScreen;
+      engine.displayLoadingUI();
+      const girl =
+        // 'https://raw.githubusercontent.com/YUFOHON/babylon/main/assets/girl/girl_laugh_idle_angry_clapping.glb';
+        'https://raw.githubusercontent.com/YUFOHON/babylon/main/assets/girl/realGirl.glb';
 
+      SceneLoader.LoadAsync(girl, undefined, engine).then(loadScene => {
+        engine.hideLoadingUI();
 
-        SceneLoader.LoadAsync(girl, undefined, engine).then(loadScene => {
-          engine.hideLoadingUI();
-          // const morphTarget_mouthOpen = loadScene
-          //   .getMeshById('Wolf3D_Head.001')!
-          //   .morphTargetManager?.getTarget(0);
-          // const morphTarget_smile = loadScene
-          //   .getMeshById('Wolf3D_Head.001')!
-          //   .morphTargetManager?.getTarget(1);
-          // morphTarget_mouthOpen!.influence = 1;
-          // morphTarget_smile!.influence = 1;
-          loadScene.clearColor = new Color4(1, 1, 1, 1);
-          setScene(loadScene);
-          loadScene.createDefaultCameraOrLight(true, undefined, true);
-          (loadScene.activeCamera as ArcRotateCamera).alpha += Math.PI;
-          (loadScene.activeCamera as ArcRotateCamera).radius = 5;
-          setCamera(loadScene.activeCamera!);
+        // const morphTarget_mouthOpen = loadScene
+        //   .getMeshById('Wolf3D_Head.001')!
+        //   .morphTargetManager?.getTarget(0);
+        // const morphTarget_smile = loadScene
+        //   .getMeshById('Wolf3D_Head.001')!
+        //   .morphTargetManager?.getTarget(1);
+        // morphTarget_mouthOpen!.influence = 1;
+        // morphTarget_smile!.influence = 1;
+        loadScene.clearColor = new Color4(1, 1, 1, 1);
+        setScene(loadScene);
+        loadScene.createDefaultCameraOrLight(true, undefined, true);
+        (loadScene.activeCamera as ArcRotateCamera).alpha += Math.PI;
+        (loadScene.activeCamera as ArcRotateCamera).radius = 5;
+        setCamera(loadScene.activeCamera!);
 
-          // const laughAnim = loadScene.getAnimationGroupByName('laugh')!;
-          // const angryAnim = loadScene.getAnimationGroupByName('angry')!;
-          // const clappingAnim = loadScene.getAnimationGroupByName('Clapping')!;
-          // const idleAnim = loadScene.getAnimationGroupByName('idle')!;
+        // const talkAnim = loadScene.getAnimationGroupByName('Female_Talk')!;
+        // const laughAnim = loadScene.getAnimationGroupByName('laugh')!;
+        // const angryAnim = loadScene.getAnimationGroupByName('angry')!;
+        // const clappingAnim = loadScene.getAnimationGroupByName('Clapping')!;
+        // const idleAnim = loadScene.getAnimationGroupByName('idle')!;
+        // loadScene.meshes.forEach(mesh => {
+        //   mesh.actionManager = new ActionManager();
+        //   mesh.actionManager.registerAction(
+        //     new ExecuteCodeAction(ActionManager.OnPickTrigger, function () {
+        //       console.warn('CLICK');
+        //       talkAnim.start(true, 1.0, talkAnim.from, talkAnim.to, false);
+        //     }),
+        //   );
+        // });
 
-          // loadScene.meshes.forEach(mesh => {
-          //   mesh.actionManager = new ActionManager();
+        // loadScene.meshes.forEach(mesh => {
+        //   mesh.actionManager = new ActionManager();
 
-          //   mesh.actionManager.registerAction(
-          //     new ExecuteCodeAction(ActionManager.OnPickTrigger, function () {
-          //       console.warn('CLICK');
-          //       laughAnim.start(true, 1.0, laughAnim.from, laughAnim.to, false);
-          //     }),
-          //   );
-          //   mesh.actionManager.registerAction(
-          //     new ExecuteCodeAction(
-          //       ActionManager.OnPointerOverTrigger,
-          //       function () {
-          //         // console.warn('HOVER');
-          //       },
-          //     ),
-          //   );
-          //   mesh.actionManager.registerAction(
-          //     new ExecuteCodeAction(
-          //       ActionManager.OnPointerOutTrigger,
-          //       function () {
-          //         // console.warn('HOVER EXIT');
-          //       },
-          //     ),
-          //   );
-          // });
+        //   mesh.actionManager.registerAction(
+        //     new ExecuteCodeAction(ActionManager.OnPickTrigger, function () {
+        //       console.warn('CLICK');
+        //       laughAnim.start(true, 1.0, laughAnim.from, laughAnim.to, false);
+        //     }),
+        //   );
+        //   mesh.actionManager.registerAction(
+        //     new ExecuteCodeAction(
+        //       ActionManager.OnPointerOverTrigger,
+        //       function () {
+        //         // console.warn('HOVER');
+        //       },
+        //     ),
+        //   );
+        //   mesh.actionManager.registerAction(
+        //     new ExecuteCodeAction(
+        //       ActionManager.OnPointerOutTrigger,
+        //       function () {
+        //         // console.warn('HOVER EXIT');
+        //       },
+        //     ),
+        //   );
+        // });
 
-          console.log('MODELS LOADED');
-        });
-      }
-      console.log('exit model component');
-    
-
+        console.log('MODELS LOADED');
+      });
+    }
+    console.log('exit model component');
   }, [engine]);
-
 
   return (
     <>
-      <SafeAreaView style={{flex: 1, backgroundColor: 'white'}}>
+      <SafeAreaView style={{flex: 1}}>
         <View style={props.style}>
           <View style={{flex: 1}}>
+            {isLoading ? (
+              <View
+                style={{
+                  flex: 1,
+                  justifyContent: 'flex-end',
+                  alignItems: 'center',
+                }}>
+                <Text>Loading...</Text>
+                <ActivityIndicator size={50} color="#000000" />
+              </View>
+            ) : (
+              <></>
+            )}
             <EngineView
               camera={camera}
               displayFrameRate={true}
