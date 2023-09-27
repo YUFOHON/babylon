df00c87e0046f17108dfc94f71a9f4a311514456
Yu Fo Hon <117070313+YUFOHON@users.noreply.github.com>
2023-09-28 00:12:32 +0800

    test

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -2,14 +2,15 @@ import React from 'react';
 // import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Voices from '../components/VoiceInput';
 import  Model  from '../components/model/Model';
-
+import Cv from '../components/Cv/Cv';
 
 
 const HomeScreen: React.FC = () => {
   return (
     <>
+    {/* <Cv /> */}
     <Model style={{flex: 1}} />
-    <Voices  />
+    {/* <Voices  /> */}
     </>
   );
 };
